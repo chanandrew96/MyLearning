fd7970418f92e6f3383e78d45672191cd80230e5
Andrew Chan <chanandrew96@yahoo.com.hk>
2023-05-17 16:51:19 +0800

    Update Database_File_Latency_Check.sql

--- MSSQL/LatencyCheck/Database_File_Latency_Check.sql
+++ MSSQL/LatencyCheck/Database_File_Latency_Check.sql
@@ -1,9 +1,45 @@
-SELECT DB_NAME (a.database_id) AS dbname,
-	a.io_stall / NULLIF (a.num_of_reads + a.num_of_writes, 0) AS average_tot_latency,
-	Round ((a.size_on_disk_bytes / square (1024.0)), 1) AS size_mb,
-	b.physical_name AS [fileName]
-FROM sys.dm_io_virtual_file_stats(NULL, NULL) AS a,
-	sys.master_files AS b
-WHERE a.database_id = b.database_id
-	AND a.FILE_ID = b.FILE_ID
-ORDER BY average_tot_latency DESC
+SELECT
+	DB_NAME ([vfs].[database_id]) AS [DB],
+	[ReadLatency] =
+		CASE WHEN [num_of_reads] = 0
+			THEN 0 ELSE ([io_stall_read_ms] / [num_of_reads]) END,
+	[WriteLatency] =
+		CASE WHEN [num_of_writes] = 0
+			THEN 0 ELSE ([io_stall_write_ms] / [num_of_writes]) END,
+	[Latency] =
+		CASE WHEN ([num_of_reads] = 0 AND [num_of_writes] = 0)
+			THEN 0 ELSE ([io_stall] / ([num_of_reads] + [num_of_writes])) END,
+	[Latency Desc] = 
+		CASE 
+			WHEN ([num_of_reads] = 0 AND [num_of_writes] = 0) THEN 'N/A' 
+			ELSE 
+				CASE WHEN ([io_stall] / ([num_of_reads] + [num_of_writes])) < 2 THEN 'Excellent'
+					WHEN ([io_stall] / ([num_of_reads] + [num_of_writes])) < 6 THEN 'Very good'
+					WHEN ([io_stall] / ([num_of_reads] + [num_of_writes])) < 11 THEN 'Good'
+					WHEN ([io_stall] / ([num_of_reads] + [num_of_writes])) < 21 THEN 'Poor'
+					WHEN ([io_stall] / ([num_of_reads] + [num_of_writes])) < 101 THEN 'Bad'
+					WHEN ([io_stall] / ([num_of_reads] + [num_of_writes])) < 501 THEN 'Yikes!'
+				ELSE 'YIKES!!'
+				END 
+		END, 
+	[AvgBPerRead] =
+		CASE WHEN [num_of_reads] = 0
+			THEN 0 ELSE ([num_of_bytes_read] / [num_of_reads]) END,
+	[AvgBPerWrite] =
+		CASE WHEN [num_of_writes] = 0
+			THEN 0 ELSE ([num_of_bytes_written] / [num_of_writes]) END,
+	[AvgBPerTransfer] =
+		CASE WHEN ([num_of_reads] = 0 AND [num_of_writes] = 0)
+			THEN 0 ELSE
+				(([num_of_bytes_read] + [num_of_bytes_written]) /
+				([num_of_reads] + [num_of_writes])) END,
+	LEFT ([mf].[physical_name], 2) AS [Drive],
+	[mf].[physical_name],
+	Round (([vfs].[size_on_disk_bytes] / square (1024.0)), 1) AS size_mb
+FROM
+   sys.dm_io_virtual_file_stats (NULL,NULL) AS [vfs]
+   JOIN sys.master_files AS [mf]
+   ON [vfs].[database_id] = [mf].[database_id]
+      AND [vfs].[file_id] = [mf].[file_id]
+ORDER BY [Latency] DESC
+GO
